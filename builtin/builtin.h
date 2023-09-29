ff6c0d8bd9323ee0d9e97ed99a4bab11e8e5b9ac
brettleclerc13 <brettleclerc13@gmail.com>
2023-09-29 12:16:55 +0200

    ft_cd updates

--- builtin/builtin.h
+++ builtin/builtin.h
@@ -6,7 +6,7 @@
 /*   By: brettleclerc <brettleclerc@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 15:58:50 by brettlecler       #+#    #+#             */
-/*   Updated: 2023/09/26 17:29:14 by brettlecler      ###   ########.fr       */
+/*   Updated: 2023/09/29 12:15:19 by brettlecler      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@
 # include <stdbool.h>
 # include <stdarg.h>
 # include <stddef.h>
+# include <limits.h>
 
 # define true 1
 # define false 0
 
 # include "../Libft/libft.h"
 
-int	ft_cd(int argc, char **argv, char **envp);
-char	*get_home_dir(char **envp);
-int	ft_echo(char **argv);
+int		ft_cd(int argc, char **argv, char **envp);
+int		ft_cd_contd(char *dir, char *cwd, char **envp);
+char	*get_env_value(char *var, char **envp);
+int		ft_echo(char **argv);
 
 #endif
--- builtin/ft_cd.c
+++ builtin/ft_cd.c
@@ -6,45 +6,72 @@
 /*   By: brettleclerc <brettleclerc@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 13:53:41 by brettlecler       #+#    #+#             */
-/*   Updated: 2023/09/26 17:40:08 by brettlecler      ###   ########.fr       */
+/*   Updated: 2023/09/29 12:15:02 by brettlecler      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include "minishell.h"
 #include "builtin.h"
 
-char	*get_home_dir(char **envp)
+char	*get_env_value(char *var, char **envp)
 {
 	int	i;
+	int	str_len;
 	
 	i = -1;
+	str_len = ft_strlen(var);
 	while (envp[++i])
-		if (!ft_strncmp(envp[i], "HOME", 4))
-			return (envp[i] + 5);
-	return (0);
+		if (!ft_strncmp(envp[i], var, str_len))
+			return (envp[i] + str_len + 1);
+	return (NULL);
 }
 
-int	ft_cd(int argc, char **argv, char **envp)
+int	ft_cd_contd(char *dir, char *cwd, char **envp)
 {
-	char	*dir;
-
-	if (argc == 2)
-	{
-		dir = get_home_dir(envp);
-		if (!dir)
-		{
-			ft_putstr_fd("cd: Home directory not listed in environment\n", 2);
-			return (1);
-		}
-	}
-	else
-		dir = argv[2];
-	printf("dir:- %s\n", dir);
 	if (chdir(dir))
 	{
 		ft_putstr_fd("cd error: ", 2);
 		perror(dir);
 		return (1);
 	}
+	if (!get_env_value("OLD_PWD=", envp))
+		add_env_value("OLD_PWD=", cwd, envp);
+	else
+		update_env_value("OLD_PWD=", cwd, envp);
+	update_env_value("PWD=", dir, envp);
 	return (0);
 }
+
+int	ft_cd(int argc, char **argv, char **envp)
+{
+	char	*dir;
+	char	cwd[PATH_MAX];
+
+	if (!getcwd(cwd, sizeof(cwd)))
+	{
+		ft_putstr_fd("cwd: Cannot retrieve cwd", 2);
+		perror(cwd);
+		return (1);
+	}
+	if (argc == 2)
+	{
+		dir = get_env_value("HOME=", envp);
+		if (!dir)
+		{
+			ft_putstr_fd("cd: Home directory not listed in env\n", 2);
+			return (1);
+		}
+	}
+	else
+		dir = argv[2];
+	if (ft_cd_contd(dir, cwd, envp))
+		return (1);
+	return (0);
+}
+
+/*
+pwd
+oldpwd
+	printf("dir: %s\n", dir);
+	printf("argv[2]: %s\n", argv[2]);
+*/
